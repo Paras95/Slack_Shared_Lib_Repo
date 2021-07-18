78381f9d739c113c88eb72452bd1b43ef4eb0a8e
Paras95 <68475234+Paras95@users.noreply.github.com>
2021-07-18 14:58:10 +0530

    Update parasSrc.groovy

--- src/org/foo/parasSrc.groovy
+++ src/org/foo/parasSrc.groovy
@@ -1,1 +1,18 @@
 
+package org.foo
+class parasSrc implements Serializable {
+  def script
+  def steps
+  parasSrc(steps, script) {
+    this.steps = steps
+    this.script = script
+  }
+  def mvn(args) {
+    steps.sh "${steps.tool 'maven'}/bin/mvn -o ${args}"
+  }
+  
+  def env(){
+    steps.echo "build number----------${script.env.BUILD_NUMBER}"
+  }
+  
+}
