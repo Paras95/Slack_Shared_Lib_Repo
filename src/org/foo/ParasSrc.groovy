1d99930a99fb607c2c6f87f7374fcc35339b4faf
paras <sharma234.paras@gmail.com>
2022-01-16 02:25:06 +0530

    commitd

--- src/org/foo/ParasSrc.groovy
+++ src/org/foo/ParasSrc.groovy
@@ -16,9 +16,9 @@ class ParasSrc implements Serializable {
     steps.sh 'mvn install'
     steps.sh 'mvn test'
     steps.echo "testing.......1,2,3"
-    archiveArtifacts artifacts: 'target/surefire-reports/*.xml', fingerprint: true
+    this.steps.archiveArtifacts artifacts: 'target/surefire-reports/*.xml', fingerprint: true
     def test = junit 'target/surefire-reports/*.xml'    
-    slackSend (
+    this.steps.slackSend (
                 channel: "#general",
                 color: '#007D00',
                 message: "\n *Test Summary* - ${test.totalCount}, Failures: ${test.failCount}, Skipped: ${test.skipCount}, Passed: ${test.passCount}"
