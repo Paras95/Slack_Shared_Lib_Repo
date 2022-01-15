074b09a4d22a79116155def8e5f37efdfe2fa59c
paras <sharma234.paras@gmail.com>
2022-01-16 00:39:18 +0530

    commited...

--- src/org/foo/ParasSrc.groovy
+++ src/org/foo/ParasSrc.groovy
@@ -13,8 +13,9 @@ class ParasSrc implements Serializable {
   
   def env(){
     steps.echo "build number----------${script.env.BUILD_NUMBER}"
-    steps.echo "testing...."
+    
     steps.sh 'mvn test'
+    steps.echo "testing.......1,2,3"
     archiveArtifacts artifacts: '**/target/surefire-reports/*.xml', fingerprint: true
     def test = junit '**/target/surefire-reports/*.xml'    
                 slackSend (
